58c3b9a273d1b3dcf75596330d15fc6fbd9d25b8
Ilia Konnov <iliakonnov@gmail.com>
2018-04-20 10:58:36 +0300

    Сделал сортировку сообщений в канале по дате и времени. Closes #2

--- Bot/Channel.py
+++ Bot/Channel.py
@@ -1,5 +1,6 @@
 import abc
 from time import time
+from typing import List
 
 from Bot import Config, Message
 
@@ -18,30 +19,54 @@ class DisabledChannel(BaseChannel):
 class NormalChannel(BaseChannel):
     def __init__(self):
         import telepot
+        self.messages: List[Message.Message] = []
         self.channel_msg = ''
         self.channel_time = 0
         self.channel_bot = telepot.Bot(Config.bot_token)
 
     def send_to_channel(self, message: Message.Message = None):
-        if message is None:
-            msg = ''
-        else:
-            from_name = '`[{:%Y.%m.%d %H:%M:%S} UTC] {}:`\n'.format(
-                message.date, 'Бот' if message.is_from_bot else 'Я'
+        if message is not None:
+            self.messages.append(message)
+            self.messages.sort(key=lambda x: x.date)
+
+        msg = self._prepare_msg()
+
+        send_now = (
+                (time() - self.channel_time > Config.channel_delay)
+                or msg[2]  # length limited
+        )
+
+        if send_now and msg[0]:
+            self.channel_bot.sendMessage(
+                Config.channel_id,
+                msg[0],
+                parse_mode='Markdown',
+                disable_notification=True
             )
-            msg = '\n\n' + from_name + message.text
+            self.messages = self.messages[msg[1]:]  # offset
+            self.channel_time = time()
 
-        t = time()
-        if t - self.channel_time > Config.channel_delay:
-            send_now = True
-            self.channel_time = t
-        else:
-            new_len = len(self.channel_msg) + len(msg)
-            send_now = new_len > 4096
+    def _prepare_msg(self):
+        if not self.messages:
+            return '', 0, False
 
-        if send_now and self.channel_msg:
-            self.channel_bot.sendMessage(Config.channel_id, self.channel_msg, parse_mode='Markdown',
-                                         disable_notification=True)
-            self.channel_msg = ''
-        self.channel_msg += msg
-        self.channel_msg.strip('\n')
+        msg = ''
+        length_limited = False
+        offset = 0
+        m = self.messages[offset]
+        while True:
+            from_name = '`[{:%Y.%m.%d %H:%M:%S} UTC] {}:`\n'.format(
+                m.date, 'Бот' if m.is_from_bot else 'Я'
+            )
+            new_msg = msg + '\n\n' + from_name + m.text
+            new_msg.strip('\n')
+            if len(new_msg) > 4096:
+                length_limited = True
+                break
+            elif offset + 1 < len(self.messages):
+                msg = new_msg
+                offset += 1
+                m = self.messages[offset]
+            else:
+                break
+        return msg, offset, length_limited
